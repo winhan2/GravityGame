459f6da29385d6a5b4f64a696248c6129fda9d1c
lxc <1951381703@qq.com>
2024-06-14 20:13:19 +0800

    Name GravityGame This is a 2D gravity game.
    Version 1.0
    OpenSource Agreement is GPLv2.0

---
+++ test.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+# import os
+#
+# for i in range(2, 367):
+#
+#     os.rename(fr'G:\David\PhotoShop\Project\螺纹\螺纹{i}.psd', fr'G:\David\PhotoShop\Project\螺纹\lw{i}.psd')
+# print('ok')
+
+# for i in range(2, 367):
+#     os.remove(fr'G:\David\PhotoShop\Project\螺纹\lw{i}.psd')
+# print('ok')
+
+
+# from PIL import Image
+#
+# im = Image.open(r'G:\David\PhotoShop\Project\螺纹\seed.jpg')
+# for i in range(1, 366):
+#     im2 = im.rotate(i * -1)
+#     im2.save(fr'G:\David\PhotoShop\Project\螺纹\{i}.jpg')
