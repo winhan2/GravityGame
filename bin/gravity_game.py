b352d6694510098911a92af180972cb2c901fa2b
lxc <1951381703@qq.com>
2024-06-14 21:16:40 +0800

    Name GravityGame This is a 2D gravity game.
    Version 1.0
    OpenSource Agreement is GPLv2.0

--- bin/gravity_game.py
+++ bin/gravity_game.py
@@ -3,3 +3,10 @@
 import logger
 
 l = logger.Logger()
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- bin/gui.py
+++ bin/gui.py
@@ -1,1 +1,15 @@
 # coding: utf-8
+
+import pygame
+pygame.init()
+
+class Index:
+    def __init__(self):
+        width, height = 800, 600
+        background = (255, 255, 255)
+        self.screen = pygame.display.set_mode((width, height))
+        self.screen.fill(background)
+        pygame.display.set_caption('GravityGame')
+
+        pygame.display.flip()
+
